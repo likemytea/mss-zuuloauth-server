1ab319563ccc305620958ef875e253d022cb2485
huayu <huayu@DESKTOP-N5K2GI1>
2018-09-04 10:30:53 +0800

    asdfa

---
+++ src/main/java/com/chenxing/msszuuloauthserver/MssZuuloauthServerApplication.java
@@ -0,0 +1,12 @@
+package com.chenxing.msszuuloauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssZuuloauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssZuuloauthServerApplication.class, args);
+	}
+}
